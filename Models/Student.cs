00a8c64be7da20f0f29c84980677abae1f3656c0
MarkAnthony <markanthonylaredo@gmail.com>
2023-05-18 08:58:04 +0800

    Refactor Student class to use record type

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,24 +1,3 @@
 ﻿namespace AspNetCoreWebMinimalApi.Models;
 
-public sealed class Student
-{
-    public Student(Guid id,
-        string name,
-        string level,
-        string section,
-        string academicYear)
-    {
-        Id = id;
-        Name = name;
-        Level = level;
-        Section = section;
-        AcademicYear = academicYear;
-    }
-
-    public Guid Id { get; init; }
-    public string Name { get; init; }
-    public string Level { get; init; }
-    public string Section { get; init; }
-    public string AcademicYear { get; init; }
-
-}
+public sealed record Student(Guid Id, string Name, string Level, string Section, string AcademicYear);
